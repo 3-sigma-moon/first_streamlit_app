f9c12957c2c079602693de7602c448b12c003d5f
3-sigma-moon <82468352+3-sigma-moon@users.noreply.github.com>
2023-02-21 15:52:42 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit
 
 streamlit.title("My parents new healthy dinner 🥗")
+
+streamlit.header("------Breakfast Menu------")
+
+streamlit.text("-> Overnight oats")
+streamlit.text("-> Loaded avocado toast")
+streamlit.text("-> Yogurt and fruit parfaits")
+streamlit.text("-> Broccoli and cheese egg bake")
+
+
+
